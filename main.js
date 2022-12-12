76d1cfd9c18992b6a0ba631e91803cf08db9d849
Matthew McDonald <yerwhile@gmail.com>
2022-12-12 09:36:49 -0600

    functions deconstruct

---
+++ main.js
@@ -0,0 +1,25 @@
+const fetchPaper = () => {
+    console.log("I fetched a piece of A3 paper");
+}
+
+const foldPaper = () => {
+    console.log("I folded the paper in half");
+}
+
+const cleanPen = () => {
+    console.log("I cleaned my pen");
+}
+
+const fillPen = () => {
+    console.log("I cleaned my pen");
+}
+
+const writeLetter = () => {
+    console.log("I wrote a letter");
+}
+
+fetchPaper();
+foldPaper();
+cleanPen();
+fillPen();
+writeLetter();
